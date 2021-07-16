8155c4cf5bd935619aae40a78cbdfe5725597d78
John Pajarillo <git.japajarillo@gmail.com>
2021-07-16 14:03:37 +0800

    Client Interface

---
+++ docs/precache-manifest.a0f515eb3eedb069a10ae287d5569715.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e2a5cc73be2ac837a4009b273a0195",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a493924f1528ef7a14f7",
+    "url": "/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "de55e71974b5daf87d5b",
+    "url": "/static/js/2.d73d354d.chunk.js"
+  },
+  {
+    "revision": "a493924f1528ef7a14f7",
+    "url": "/static/js/main.9772b4e4.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
